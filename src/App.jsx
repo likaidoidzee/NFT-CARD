c4628e053ae81a085dd2cb332b5db36fa8cc650d
Your Name <youremail@example.com>
2025-10-16 20:39:55 +0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="card">
+      <img src="./Rectangle.png" />
+      <h1>equlibarum #3429</h1>
+      <p>Our Equilibrium collection promotes balance and calm.</p>
+      <div>
+      <div className="contentDiv">
+
+      </div>
+
+      <div className="content1">
+        <img className="icon1" src="./shape.png"/>
+        <p className="text1">0.041 ETH</p>
+      
+      </div>
+      <div className="content2">
+        <img  className="icon2" src="./clock.png"/>
+      <p className="text2"> 3 days left</p>
+      </div>
+      </div>
+      <div className="line"></div>
+      <div className="avatarDiv">
+        <img  className="avatar" src="./Oval.png"/>
+        <span>Creation of jules Wyvern</span>
+      </div>
+      </div>
+      </>
+      );
+
+      }
+
+
+export default App;
